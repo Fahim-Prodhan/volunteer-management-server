c8cbb228d3a2c3d428b69a1c83f8f87898186dbc
Fahim-Prodhan <prodhan15-4809@diu.edu.bd>
2024-05-12 17:43:25 +0600

    Delete Api create for my need volunteer post and My requested post

--- index.js
+++ index.js
@@ -9,80 +9,175 @@ const port = process.env.PORT || 5000;
 
 // middleware
 app.use(
-    cors({
-      origin: ["http://localhost:5173"],
-      credentials: true,
-    })
-  );
-  app.use(express.json());
-  app.use(cookieParser());
+  cors({
+    origin: ["http://localhost:5173"],
+    credentials: true,
+  })
+);
+app.use(express.json());
+app.use(cookieParser());
 
+const verifyToken = (req, res, next) => {
+  const token = req.cookies?.token;
+  // console.log("value of token in middleware:", token);
+  if (!token) {
+    return res.status(401).send({ message: "Not authorized" });
+  }
+  jwt.verify(token, process.env.SECRET_KEY, (err, decoded) => {
+    //error
+    if (err) {
+      // console.log(err);
+      return res.status(401).send({ message: "Unauthorized" });
+    }
+    // if token is valid then it would be decoded
+    // console.log("value of token", decoded);
+    req.user = decoded;
+  });
 
-  const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.djweinm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+  next();
+};
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.djweinm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 async function run() {
   try {
+    const volunteerCollection = client
+      .db("volunnetDB")
+      .collection("volunteerCollections");
+    const BeVolunteerCollection = client
+      .db("volunnetDB")
+      .collection("beVolunteerCollections");
 
-    const volunteerCollection = client.db('volunnetDB').collection('volunteerCollections')
+    app.post("/jwt", async (req, res) => {
+      const user = req.body;
+      // console.log(user);
+      const token = jwt.sign(user, process.env.SECRET_KEY, { expiresIn: "1h" });
+      const cookieOptions = {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        sameSite: process.env.NODE_ENV === "production" ? "none" : "strict",
+      };
+      res.cookie("token", token, cookieOptions).send({ success: true });
+    });
 
     // Volunteer
-    app.get('/volunteerPosts', async(req,res)=>{
-      const page = parseInt(req.query.page)
-      const size = parseInt(req.query.size)
-      const text = req.query.search
+    app.get("/volunteerPosts", async (req, res) => {
+      const page = parseInt(req.query.page);
+      const size = parseInt(req.query.size);
+      const text = req.query.search;
       let query = {
-        title: {$regex : text, $options:'i'}
-      }
-      const result = await volunteerCollection.find(query)
-      .skip(page*size)
-      .limit(size)
-      .sort({_id: -1}).toArray()
-      res.send(result)
-    })
+        title: { $regex: text, $options: "i" },
+      };
+      const result = await volunteerCollection
+        .find(query)
+        .skip(page * size)
+        .limit(size)
+        .sort({ _id: -1 })
+        .toArray();
+      res.send(result);
+    });
 
-    
-    app.get('/volunteerPost/:id', async(req, res)=>{
-      const id = req.params.id
-      const query =  {_id: new ObjectId(id)}
-      const result = await volunteerCollection.findOne(query)
-      res.send(result)
-    })
+    app.get("/posts", async (req, res) => {
+      const result = await volunteerCollection
+        .find()
+        .sort({ deadline: 1 })
+        .toArray();
+      res.send(result);
+    });
 
-    app.get('/postCounts', async(req, res)=>{
-      const text = req.query.search
+    app.get("/volunteerPost/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await volunteerCollection.findOne(query);
+      res.send(result);
+    });
+
+    app.get("/postCounts", async (req, res) => {
+      const text = req.query.search;
       let query = {
-        title: {$regex : text, $options:'i'}
-      }
-      const result = await volunteerCollection.countDocuments(query)
-      res.send({count: result})
-    })
+        title: { $regex: text, $options: "i" },
+      };
+      const result = await volunteerCollection.countDocuments(query);
+      res.send({ count: result });
+    });
 
-    app.post('/volunteerPost', async(req, res)=>{
-      const body = req.body
-      console.log(body);
-      const result = await volunteerCollection.insertOne(body)
-      res.send(result)
-    })
+    app.post("/volunteerPost", async (req, res) => {
+      const body = req.body;
+      // console.log(body);
+      const result = await volunteerCollection.insertOne(body);
+      res.send(result);
+    });
+
+    app.post("/beVolunteerPost/:id", async (req, res) => {
+      const id = req.params.id;
+      const body = req.body;
+      // console.log(id);
+      const result = await BeVolunteerCollection.insertOne(body);
+      const incrementQuery = { _id: new ObjectId(id) };
+      const incrementUpdate = { $inc: { volunteers_needed: -1 } };
+      await volunteerCollection.updateOne(incrementQuery, incrementUpdate);
+      res.send(result);
+    });
+
+    app.get("/myPosts", verifyToken, async (req, res) => {
+      // console.log(req.query.email);
+      // console.log('token = ', req.cookies);
+      // console.log('user in valid token', req.user)
+      if (req.query.email != req.user.email) {
+        return res.status(403).send({ message: "forbidden" });
+      }
+      let query = {};
+      if (req.query?.email) {
+        query = { email: req.query.email };
+      }
+      const result = await volunteerCollection.find(query).toArray();
+      res.send(result);
+    });
+
+    app.delete("/myPosts/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const find = await volunteerCollection.findOne(query);
+      if (find) {
+        const result = await volunteerCollection.deleteOne(query);
+        res.send(result);
+      } else {
+        const result = await BeVolunteerCollection.deleteOne(query);
+        res.send(result);
+      }
+    });
+
+    app.get("/myRequestedPosts", verifyToken, async (req, res) => {
+      if (req.query.email != req.user.email) {
+        return res.status(403).send({ message: "forbidden" });
+      }
+      let query = {};
+      if (req.query?.email) {
+        query = { volunteer_email: req.query.email };
+      }
+      const result = await BeVolunteerCollection.find(query).toArray();
+      res.send(result);
+    });
 
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
- 
   }
 }
 run().catch(console.dir);
 
-app.get('/', async(req,res)=>{
-    res.send("Testing Server")
-})
+app.get("/", async (req, res) => {
+  res.send("Testing Server");
+});
 app.listen(port, () => {
-    console.log("coffee shop running at ", port);
-  });
-  
+  console.log("coffee shop running at ", port);
+});
